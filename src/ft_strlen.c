699e7518fc4dc236f4d628d8a2418f3a061d4d46
Oussama Ezzaou <oussamaezzaou@gmail.com>
2025-03-01 15:30:53 +0000

    libft: ft_strlen() is updated

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: oezzaou <oezzaou@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 21:16:13 by oezzaou           #+#    #+#             */
-/*   Updated: 2025/02/24 13:43:12 by oezzaou          ###   ########.fr       */
+/*   Updated: 2022/10/05 13:43:12 by oezzaou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,9 +14,18 @@
 //====<[ ft_strlen: ]>==========================================================
 size_t	ft_strlen(const char *s)
 {
-	if (s == NULL)
-		return (-1);
-	if (*s)
-		return (1 + ft_strlen(++s));
-	return (0);
+  int len;
+
+  len = -1;
+  while (s[++len] != '\0')
+    ;
+  return (len);
 }
+// size_t	ft_strlen(const char *s)
+// {
+// 	if (s == NULL)
+// 		return (-1);
+// 	if (*s)
+// 		return (1 + ft_strlen(++s));
+// 	return (0);
+// }
